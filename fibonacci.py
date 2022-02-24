0a1d84afab1e394f29329a9181ad7c2949bce54c
Vipin Kumar <vipinthakur3881@gmail.com>
2022-02-24 20:29:26 +0530

    find the fibonacci number

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,17 @@
-a=int(input("enter a number:"))
-number1 =0
-number2=1
-counting=0
-if a==0:
- print("result is not possible")
-elif a==1:
- print(a)
+a = int(input("enter a number:"))
+number1 = 0
+number2 = 1
+counting = 0
+if a == 0:
+    print("result is not possible")
+elif a == 1:
+    print(a)
 else:
- print("fibonacci series")
- while counting < a:
-  print(number1)
-  result = number1 +number2
-  number1 = number2
-  number2=result
-  if number1 >=a:
-   break
-
+    print("fibonacci series")
+    while counting < a:
+        print(number1)
+        result = number1 + number2
+        number1 = number2
+        number2 = result
+        if number1 >= a:
+            break
