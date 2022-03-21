cfce2c6a02ca19317ff0ece4ca8829dbba103d34
Vipin Kumar <vipinthakur3881@gmail.com>
2022-03-21 16:06:53 +0530

    find the maximum number

---
+++ maximum_number.py
@@ -0,0 +1,12 @@
+a = int(input("Give the value of a:"))
+b = int(input("Give the value of b:"))
+
+
+def maximum(a, b):
+    if a >= b:
+        print(a)
+    else:
+        print(b)
+
+
+maximum(a, b)
